acc070a4b90ba93e5be79fa891c151eee936ebd1
elenapetkovska <work@elenapetkovska.com>
2025-08-24 00:15:55 +0200

    UCSC C++ Course, ex-11-20

---
+++ Cpp/Cpp_Course_University_of_California_Santa_Cruz/Cpp-UCSC-ex-16.cpp
@@ -0,0 +1,29 @@
+// Created by Elena Petkovska on 5.4.2025.
+//
+
+/* Cpp Course University of California Santa Cruz */
+
+#include <iostream>
+
+class B {
+public: virtual void foo(int i) {
+    std::cout << "B::foo(int) " << i << "\n";
+}
+    virtual void foo(double x) {
+    std::cout << "B::foo(double) " << x << "\n";
+}
+};
+
+class D: public B {
+public: void foo(int i) {
+    std::cout << "D::foo(int) " << i << "\n";
+}
+};
+
+int main() {
+    D d;
+    B b, * pb = & d;
+    b.foo(9.5);
+    d.foo(9.5);
+    pb -> foo(9.5);
+}
---
+++ Cpp/Cpp_Course_University_of_California_Santa_Cruz/Cpp-UCSC-ex-17.cpp
@@ -0,0 +1,27 @@
+// Created by Elena Petkovska on 5.4.2025.
+//
+
+/* Cpp Course University of California Santa Cruz */
+
+//C++ For C Programmers Part B
+//Ira Pohl  Illustrate move semantics
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i {9};
+    int* ptr_i ;
+    int &&j = move(i);
+    ptr_i = &j;
+    cout << " Semantics of varoius declarations\n";
+    cout  << "i  = "  << i << endl;
+    cout  << "j  = "  << j << endl;
+    cout  << "&i  = "  << &i << endl;
+    cout  << "ptr_i  = "  << ptr_i << endl;
+    ++i;
+    cout  << "i  = "  << i << endl;
+    cout  << "j  = "  << j << endl;
+}
