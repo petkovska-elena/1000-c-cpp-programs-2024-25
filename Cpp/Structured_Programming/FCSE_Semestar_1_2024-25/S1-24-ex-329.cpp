1768b9995eccfd3968dd8ee9e9b4883fdb08af4f
elenapetkovska <work@elenapetkovska.com>
2025-10-17 10:28:15 +0200

    FCSE C++ Semester 1, ex-326-330

---
+++ Cpp/Structured_Programming/FCSE_Semestar_1_2024-25/S1-24-ex-329.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Elena Petkovska on 14.8.25.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+}
+
+ФАКУЛТЕТ ЗА ИНФОРМАТИЧКИ НАУКИ И
+КОМПЈУТЕРСКО ИНЖЕНЕРСТВО
+СТРУКТУРНО ПРОГРАМИРАЊЕ
+Внесување на текстуални низи
+примери (
+
+Нека се дадени декларациите
+const
+int len = 10 char s len
+
+Пример со cin cin >> s
+
+Проблеми при читање со cin
+int main
+const int MAX = 12
+char name MAX
+char day MAX
+cout << "Enter your n
+cin >> name
+cout << "Enter your favourite day of the week:” << endl ;
+cin
+>> day
+cout << name << ", your favourite day is " << day <<
+".""."<< endl
+return 0; 16
+Што ако се внесе текстуална
+низа подолга од MAX знаци?
+Enter your name:
+Albert Einstein
+Enter your favourite day of the week:
+Albert, your favourite day is Einstein.
+!
---
+++ Cpp/Structured_Programming/FCSE_Semestar_1_2024-25/S1-24-ex-330.cpp
@@ -0,0 +1,46 @@
+//
+// Created by Elena Petkovska on 14.8.25.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+}
+
+ФАКУЛТЕТ ЗА ИНФОРМАТИЧКИ НАУКИ И
+КОМПЈУТЕРСКО ИНЖЕНЕРСТВО
+СТРУКТУРНО ПРОГРАМИРАЊЕ
+Внесување на текстуални низи
+примери (
+
+Нека се дадени декларациите
+const
+int len = 10 ; char s len
+
+Примери со cin getline
+cin .getline s , len
+cin .getline s , len , n
+cin .getline s , len ,
+
+Примери со cin.getline
+int main
+const int MAX = 20
+char name MAX ], day MAX
+cout << "Enter your name:” << endl
+cin .getline name , MAX
+cout << "Enter your favourite day of the week:” << endl
+cin .getline day , MAX
+cout << name << ", your favourite day is " << day << ".""."<<
+endl
+return 0 17
+Enter your name:
+Albert Einstein
+Enter your favourite day
+of the week:
+Saturday
+Albert Einstein, your favourite day is
+Saturday.
+Што ако се внесе текстуална
+низа подолга од MAX знаци?
