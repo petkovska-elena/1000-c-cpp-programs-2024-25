fe30c2aedfaad2371e4b089410083739fd6f62e5
elenapetkovska <work@elenapetkovska.com>
2025-10-18 13:25:50 +0200

    FCSE C++ Semester 1, ex-331-335

---
+++ Cpp/Structured_Programming/FCSE_Semestar_1_2024-25/S1-24-ex-335.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Elena Petkovska on 14.8.25.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+}
+
+ФАКУЛТЕТ ЗА ИНФОРМАТИЧКИ НАУКИ И
+КОМПЈУТЕРСКО ИНЖЕНЕРСТВО
+СТРУКТУРНО ПРОГРАМИРАЊЕ
+Внесување на текстуални низи
+со getchar()
+#include cstdio
+#define N 10
+char ch , str N
+int i =
+while i N 1)&&( ch getchar n'n')
+str[ i ch
+str i 0'
+
+Ги чита и празните места се додека не се
+притисне ENTER или додека не се
+прочитаат потребниот број знаци (9)
+22
+Или
+ch = cin.get
