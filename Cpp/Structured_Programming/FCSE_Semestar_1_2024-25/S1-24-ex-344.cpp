64843025428373aa8afbc1dedc0af4c584726a07
elenapetkovska <work@elenapetkovska.com>
2026-01-08 09:17:01 +0100

    Add exercise 344

---
+++ Cpp/Structured_Programming/FCSE_Semestar_1_2024-25/S1-24-ex-344.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Elena Petkovska on 14.8.25.
+//
+
+// Kvalifikaciski Zadaci
+
+/* Прочитај еден број N. Испечати ја неговата најзначајна цифра. */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string N;
+	cin >> N;
+
+	cout << N[0] << endl;
+	return 0;
+}
