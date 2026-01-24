3e3caa7a2b2245040dc7354371ccb3731969bfd1
Elena Petkovska <work@elenapetkovska.com>
2026-01-24 09:45:48 +0100

    Add exercise 359

---
+++ Cpp/Structured_Programming/FCSE_Semestar_1_2024-25/S1-24-ex-359.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Elena Petkovska on 14.8.25.
+//
+
+// Kvalifikaciski Zadaci
+
+/* Прочитај 10 цели броеви. Испечати ја најмалата вредност. */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int arr[10];
+
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> arr[10];
+	}
+
+	int min = arr[0];
+	for (int i = 1; i < 10; i++)
+	{
+		if {
+			(arr[i] < min) min = arr[i];
+		}
+	}
+	cout << min << endl;
+	return 0;
+}
