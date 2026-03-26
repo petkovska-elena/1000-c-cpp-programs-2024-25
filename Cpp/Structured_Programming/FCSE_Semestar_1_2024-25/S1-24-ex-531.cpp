51eb75c85c40b5441e43b00fc96dd083ac9aa19f
Elena Petkovska <work@elenapetkovska.com>
2026-03-26 09:13:00 +0100

    Add exercise 531

---
+++ Cpp/Structured_Programming/FCSE_Semestar_1_2024-25/S1-24-ex-531.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Elena Petkovska on 14.8.25.
+//
+
+// Programs from Lecture Slides
+/* Objekt kako argument na funkcija */
+
+#include <iostream>
+using namespace std;
+
+class myClass
+{
+	int i;
+public:
+	myClass(int n);
+	~myClass();
+
+	void set_i(int n) { i = n; }
+	int get_i() { return i; }
+};
+
+myClass::myClass(int n) { i = n; cout << "Constructing " << i << "\n"; }
+myClass::~myClass() { cout << "Destroying " << i << "\n"; }
+
+void f(myClass);
+void f(myClass ob) {
+	ob.set_i(2);
+	cout << "This is local i: " << ob.get_i() << endl;
+}
+
+int main() {
+	myClass x(1);
+	f(x);
+
+	cout << "This is i in main: ";
+	cout << x.get_i() << endl;
+	return 0;
+}
