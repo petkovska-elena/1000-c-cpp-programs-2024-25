b29eeb7d834b98df0cc2ca0e03f09869b51db8ab
Elena Petkovska <work@elenapetkovska.com>
2026-06-17 10:48:00 +0200

    Update S1-24-ex-10.cpp

--- Cpp/Structured_Programming/FCSE_Semestar_1_2024-25/S1-24-ex-10.cpp
+++ Cpp/Structured_Programming/FCSE_Semestar_1_2024-25/S1-24-ex-10.cpp
@@ -5,50 +5,44 @@
 /* Auditoriski Vezbi 1 (09.10.2024) */
 
 /* Да се напише програма каде од тастатура ќе се внесе цена на производ,
- * а потоа ќе ја испечати неговата цена со пресметан ддв.
- * ПОМОШ: ДДВ е 18% од почетната цена */
 
-#include <iostream>
+* а потоа ќе се испечати неговата цена со пресметан ДДВ.
+* ПОМОШ: ДДВ е 18% од почетната цена */
+
+#include 
 using namespace std;
 
 int main()
 {
-    float cena;
-    cout << "Vnesi ja cenata na proizvodot: ";
-    cin >> cena;
+float cena;
+cout << “Vnesete ja cenata na proizvodot: “;
+cin >> cena;
 
-    cout << "Cenata na proizvodot so vkluchen DDV e: " << cena * 1.18 << endl;
-    cout << "Taka e, skapo skapo! Zemi skeniraj ja smetkata. Ke vratish nekoj denar :)))" << endl;
-    cout << endl;
-    cout << "Kisses," << endl;
-    cout << "UJP" << endl;
+cout << "Cenata na proizvodot so vkluchen DDV iznesuva: " << cena * 1.18 << endl;
+cout << "Vi blagodarime za vnesenite podatoci." << endl;
+cout << endl;
+cout << "So pocit," << endl;
+cout << "Sistem za presmetka na DDV" << endl;
+return 0;
 
-    return 0;
 }
 
 /*
-//Variation 2: with +DDV and -DDV
-#include <iostream>
+// Variation 2: with +DDV and -DDV
+#include 
 using namespace std;
 
 int main()
 {
-    float cena;
-    cout << "Vnesi ja cenata na proizvodot: ";
-    cin >> cena;
+float cena;
+cout << “Vnesete ja cenata na proizvodot: “;
+cin >> cena;
 
-    //Cena so vkluchen DDV, zgolemuvanje za 18%
-    cout << "Cenata zgolemena za 18% e: " << cena * 1.18 << endl;
+// Cena so vkluchen DDV, zgolemuvanje za 18%
+cout << "Cenata so vkluchen DDV iznesuva: " << cena * 1.18 << endl;
+// Cena bez DDV, namaluvanje za 18%
+cout << "Cenata po namaluvanje za 18% iznesuva: " << cena * 0.82 << endl;
+return 0;
 
-    //Cena so iskluchen DDV, namaluvanje za 18%
-    cout << "Cenata namalena za 18% e: " << cena * 0.82 << endl;
-
-    return 0;
-} */
-
-
-
-
-
-
-
+}
+*/
